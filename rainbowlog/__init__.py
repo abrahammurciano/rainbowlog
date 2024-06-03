4ffbf1127be752b57cc7a93cfe14154cab4f0f55
Abraham Murciano <abrahammurciano@gmail.com>
2024-06-03 21:22:38 +0300

    remove __version__

--- rainbowlog/__init__.py
+++ rainbowlog/__init__.py
@@ -3,16 +3,6 @@
 """
 
 
-import sys
-
-
-if sys.version_info >= (3, 8):
-    import importlib.metadata as metadata
-else:
-    import importlib_metadata as metadata
-
-__version__ = metadata.version(__package__ or __name__)
-
 from ._formatter import Formatter
 
 __all__ = ("Formatter",)
